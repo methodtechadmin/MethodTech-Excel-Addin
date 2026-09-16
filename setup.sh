ccacaf34732b9ec950e90a273cf00ff0ca2f1b3a
sachin patil <sachin.patil@methodtech.in>
2026-09-16 16:05:26 +0530

    added a setup sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+# nodejs ecosystem
+export NODE="/auto/px_dev_ops/nodejs/release_20241112/node-v22.11.0-linux-x64/bin/node"
+export NPM_CONFIG_PREFIX="/auto/px_dev_ops/nodejs/release_20241112/node-v22.11.0-linux-x64/"
+export PATH="/auto/px_dev_ops/nodejs/release_20241112/node-v22.11.0-linux-x64/bin":$PATH
